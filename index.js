418326a4d97ccae842487ae3eeac43a417473d37
Richard van der Dys <richard.vanderdys@gmail.com>
2017-03-27 15:03:38 +0300

    Using jimp to save images Using memory instead of FS to store current and last images Added a timer to not allow detection right after another

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const request = require('request');
-const fs = require('fs');
+const fs = require('fs-extra');
 const jimp = require('jimp');
 const nodemailer = require('nodemailer');
+const debug = require('debug');
 const Rekognition = require('aws-sdk/clients/Rekognition');
 
 const config = {
-  intervalTime: process.env.INTERVAL_TIME ? parseInt(process.env.INTERVAL_TIME, 10) : 2000,
+  intervalTime: process.env.INTERVAL_TIME ? parseInt(process.env.INTERVAL_TIME, 10) : 1000,
   recipientEmails: process.env.RECIPIENT_EMAILS.split(','),
   cameraName: process.env.CAMERA_NAME.trim(),
   senderUser: process.env.SENDER_USER.trim(),
@@ -16,6 +16,8 @@ const config = {
   confidenceThreshold: process.env.CONFIDENCE_THRESHOLD ? parseFloat(process.env.CONFIDENCE_THRESHOLD, 10) : 80,
 };
 
+const log = debug(`WatsonCamera:${config.cameraName}:`);
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -24,9 +26,9 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const lastPath = './temp/last.jpg';
-const currentPath = './temp/current.jpg';
-const DETECT = 'DETECT';
+let lastImage = null;
+let currentImage = null;
+let lastDetectionAt = 0;
 
 const rekognition = new Rekognition({
   region: process.env.AWS_REGION,
@@ -34,7 +36,7 @@ const rekognition = new Rekognition({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-function sendDetection(labels) {
+function sendDetection(labels, detectedImagePath) {
   const message = `We have detected "${labels.join(', ')}" from the "${config.cameraName}" camera.`;
   const mailOptions = {
     from: '"Watson" <watson.dijs@gmail.com>',
@@ -48,7 +50,7 @@ function sendDetection(labels) {
     `,
     attachments: [{
       filename: 'current.jpg',
-      path: currentPath,
+      path: detectedImagePath,
       cid: 'detection',
     }],
   };
@@ -62,38 +64,34 @@ function sendDetection(labels) {
   });
 }
 
-function saveSnapshot(path) {
-  return new Promise((resolve, reject) => {
-    const stream = fs.createWriteStream(path);
-    stream.on('error', reject);
-    stream.on('finish', resolve);
-    request(config.snapshotUrl).pipe(stream);
-  });
-}
-
 function snapshotsSimilar() {
-  return Promise.all([lastPath, currentPath].map(path => jimp.read(path)))
-    .then(([aImage, bImage]) => {
-      const diff = jimp.diff(aImage, bImage);
-      return diff.percent < config.diffThreshold;
-    });
+  const diff = jimp.diff(currentImage, lastImage);
+  return diff.percent < config.diffThreshold;
 }
 
 function handleSnapshot() {
-  if (fs.existsSync(currentPath)) {
-    fs.renameSync(currentPath, lastPath);
+  if (currentImage) {
+    lastImage = currentImage;
   }
-  if (fs.existsSync(lastPath)) {
-    return saveSnapshot(currentPath).then(() => true);
+  if (lastImage) {
+    return jimp.read(config.snapshotUrl)
+      .then(image => {
+        currentImage = image;
+        return true;
+      });
   }
-  return saveSnapshot(lastPath).then(() => false);
+  return jimp.read(config.snapshotUrl)
+    .then(image => {
+      lastImage = image;
+      return false;
+    });
 }
 
-function getLabels() {
+function getLabels(imagePath) {
   return new Promise((resolve, reject) => {
     const params = {
       Image: {
-        Bytes: fs.readFileSync(currentPath),
+        Bytes: fs.readFileSync(imagePath),
       },
     };
     rekognition.detectLabels(params, (err, data) => {
@@ -108,29 +106,54 @@ function getLabels() {
   });
 }
 
+function saveDetection() {
+  const detectionPath = `./temp/detection-${Date.now()}.jpg`;
+  return new Promise((resolve, reject) => {
+    currentImage.write(detectionPath, err => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(detectionPath);
+    });
+  });
+}
+
 async function handleDetect() {
-  const shouldContinue = await handleSnapshot();
-  if (!shouldContinue) {
-    return false;
+  try {
+    const shouldContinue = await handleSnapshot();
+    if (!shouldContinue) {
+      log('Should not continue');
+      return false;
+    }
+    if (Date.now() - lastDetectionAt < 2000) {
+      log('Too early after a detection.');
+      return;
+    }
+    const similar = snapshotsSimilar();
+    if (similar) {
+      log('Current image is similar to last');
+      return false;
+    }
+    lastDetectionAt = Date.now();
+    const detectionPath = await saveDetection();
+    const labels = await getLabels(detectionPath);
+    if (labels.length === 0) {
+      log('Detected something, but could not find a label for it');
+      return false;
+    }
+    const messageId = await sendDetection(labels, detectionPath);
+    log(`Detected "${labels.join(', ')}".`);
+    fs.remove(detectionPath);
+    return true;
+  } catch(e) {
+    log('Handle Detect Error', e.stack);
   }
-  const similar = await snapshotsSimilar();
-  if (similar) {
-    return false;
-  }
-  const labels = await getLabels();
-  if (labels.length === 0) {
-    return false;
-  }
-  const messageId = await sendDetection(labels);
-  console.log(`Sent ${messageId}.`);
+  return false;
 }
 
 function loop() {
-  try {
-    handleDetect();
-  } catch(e) {
-    console.log('Handle Detect Error', e);
-  }
+  log('Watching for changes...');
+  let detectionMade = handleDetect();
   setTimeout(loop, config.intervalTime);
 }
 
